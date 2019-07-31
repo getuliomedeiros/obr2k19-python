00b4abf76510bebebbb41daf4abfcc783a40b500
Getúlio Medeiros <43413275+getuliomedeiros@users.noreply.github.com>
2019-07-31 19:27:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,61 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from threading import Thread
 
-from followLine import FollowLine
-from deflectObstacle import DeflectObstacle
+brick.sound.beep()
 
-# Follow line
-follow_line = FollowLine(Motor(Port.A),Motor(Port.B),ColorSensor(Port.S1),ColorSensor(Port.S4),UltrasonicSensor(Port.S2))
-follow_line.follow()
+brick.display.clear()
 
-# Deflect obstacle
-deflect_obstacle = DeflectObstacle(Motor(Port.A),Motor(Port.B),ColorSensor(Port.S1),ColorSensor(Port.S4))
-deflect_obstacle.deflect()
+# Engine declaration
+motorDirectionLeft = Motor(Port.A)
+#motorRideUp = Motor(Port.B)
+#motorGoDown = Motor(Port.C)
+motorDirectionRight = Motor(Port.D)
+
+# Sensor declaration
+sensorColorLeft = ColorSensor(Port.S1)
+sensorUltrassonicLeft = UltrasonicSensor(Port.S2)
+#sensorUltrassonicRight = UltrasonicSensor(Port.S3)
+sensorColorRight = ColorSensor(Port.S4)
+
+#Movements
+motorMovementForward = DriveBase(motorDirectionLeft,motorDirectionRight,30,30)
+motorMovementRigth = DriveBase(motorDirectionLeft,motorDirectionRight,-20,32)
+motorMovementLeft = DriveBase(motorDirectionLeft,motorDirectionRight,20,-32)
+
+# Function follow track
+def followTrack():
+
+    if(sensorColorLeft.reflection() < 30 and sensorColorRight.reflection() > 30):
+        brick.display.text("Right", (60, 50))
+        motorMovementRigth.drive(0,128)
+        brick.display.clear()
+        wait(1)
+        
+    elif (sensorColorLeft.reflection() > 30 and sensorColorRight.reflection() < 30):
+        brick.display.text("Left", (60, 50))
+        motorMovementLeft.drive(0,-128)
+        brick.display.clear()
+        wait(1)
+
+    elif(sensorColorLeft.reflection() > 30 and sensorColorLeft.reflection() > 30):
+        brick.display.text("White", (60, 50))
+        motorMovementForward.drive(60,0)
+        brick.display.clear()
+    
+    else:
+        brick.display.text("Black", (60, 50))
+        motorMovementForward.drive(20,0)
+        brick.display.clear()
+# Function deflect obstacle
+#def deflectObstacle():
+
+# Function rescue
+#def rescue():
+
+# Function detects gray
+#def detectsGray():
+
+def main():
+    while True:
+        followTrack()
+main()
