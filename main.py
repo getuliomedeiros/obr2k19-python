ef609cc44c020e9538705bdc34b88c34e93b1f19
Getúlio Medeiros <marceloremigio.bp@gmail.com>
2019-08-01 20:13:03 -0300

    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # Informática - 201513530194
 # IFPB - Campus Picuí
 
-# Library import
+# Library import 
 from pybricks import ev3brick as brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
@@ -13,62 +13,224 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from threading import Thread
 
-brick.sound.beep()
-
+#brick.sound.beep()
 brick.display.clear()
 
-# Engine declaration
+# Engine declaration --------------------------------------------------------------------
 motorDirectionLeft = Motor(Port.A)
 #motorRideUp = Motor(Port.B)
 #motorGoDown = Motor(Port.C)
 motorDirectionRight = Motor(Port.D)
 
-# Sensor declaration
+# Sensor declaration --------------------------------------------------------------------
 sensorColorLeft = ColorSensor(Port.S1)
-sensorUltrassonicLeft = UltrasonicSensor(Port.S2)
-#sensorUltrassonicRight = UltrasonicSensor(Port.S3)
+sensorUltrassonicFront = UltrasonicSensor(Port.S2)
+#sensorUltrassonicSide = UltrasonicSensor(Port.S3)
 sensorColorRight = ColorSensor(Port.S4)
 
-#Movements
+#Movements --------------------------------------------------------------------------------
 motorMovementForward = DriveBase(motorDirectionLeft,motorDirectionRight,30,30)
-motorMovementRigth = DriveBase(motorDirectionLeft,motorDirectionRight,-20,32)
-motorMovementLeft = DriveBase(motorDirectionLeft,motorDirectionRight,20,-32)
+motorMovementTurn = DriveBase(motorDirectionLeft,motorDirectionRight,20,-32)
 
-# Function follow track
+# Function detect green -------------------------------------------------------------------
+def detectGreenRight():
+
+    contGreen = 0
+    motorMovementTurn.drive(-20,0)
+    wait(100)
+    motorMovementForward.drive(0,0)
+    wait(10)
+
+    print("R1")
+    for i in range(500):
+        if sensorColorRight.color() == 2:
+            contGreen += 1
+        else:
+            contGreen += 0
+
+    print("R2")
+    for i in range(500):
+        if sensorColorRight.color() == 2:
+            contGreen += 1
+        else:
+            contGreen += 0
+
+    if contGreen >= 999:
+        print("Green")
+        motorMovementForward.drive(100,0)
+        brick.display.text("Right Green", (60, 50))
+        wait(400)
+        motorMovementTurn.drive(0,-256)
+        wait(256)
+        motorMovementTurn.drive(0,-128)
+        while sensorColorLeft.reflection() > 30:
+            wait(10) 
+        brick.display.clear()
+    contGreen = 0
+
+def detectGreenLeft():
+
+    contGreen = 0
+    motorMovementTurn.drive(20,0)
+    wait(100)
+    motorMovementForward.drive(0,0)
+    wait(10)
+
+    print("L1")
+    for i in range(500):
+        if sensorColorLeft.color() == 2:
+            contGreen += 1
+        else:
+            contGreen += 0
+
+    print("L2")
+    for i in range(500):
+        if sensorColorLeft.color() == 2:
+            contGreen += 1
+        else:
+            contGreen += 0
+
+    if contGreen >= 999:
+        print("Green")
+        motorMovementForward.drive(100,0)
+        wait(400)
+        brick.display.text("Left Green", (60, 50))
+        motorMovementTurn.drive(0,256)
+        wait(256)
+        motorMovementTurn.drive(0,128)
+        while sensorColorRight.reflection() > 30:
+            wait(10)
+        brick.display.clear()
+    contGreen = 0
+
+def doubleGreen():
+    contGreen = 0
+    motorMovementTurn.drive(20,0)
+    wait(100)
+    motorMovementForward.drive(0,0)
+    wait(10)
+
+    print("LR")
+    for i in range(500):
+        if sensorColorLeft.color() == 2 and sensorColorRight.color() == 2:
+            contGreen += 1
+        else:
+            contGreen += 0
+
+    print("LR")
+    for i in range(500):
+        if sensorColorLeft.color() == 2 and sensorColorRight.color() == 2:
+            contGreen += 1
+        else:
+            contGreen += 0
+    
+    if contGreen >= 999:
+        print("Green")
+        brick.display.text("Left Green", (60, 50))
+        motorMovementForward.drive(-100,0)
+        wait(512)
+        motorMovementTurn.drive(0,128)
+        wait(1256)
+        motorMovementTurn.drive(0,128)
+        while sensorColorRight.reflection() > 30:
+            wait(10)
+        brick.display.clear()
+    contGreen = 0
+
+# Function follow track -------------------------------------------------------------------
 def followTrack():
 
-    if(sensorColorLeft.reflection() < 30 and sensorColorRight.reflection() > 30):
-        brick.display.text("Right", (60, 50))
-        motorMovementRigth.drive(0,128)
-        brick.display.clear()
-        wait(1)
-        
-    elif (sensorColorLeft.reflection() > 30 and sensorColorRight.reflection() < 30):
-        brick.display.text("Left", (60, 50))
-        motorMovementLeft.drive(0,-128)
-        brick.display.clear()
-        wait(1)
+    if sensorColorLeft.reflection() < 30 and sensorColorRight.reflection() > 30:
+        if sensorColorLeft.color() == 2:
+            contGreen = 0
+            motorMovementForward.drive(0,0)
+            wait(1)
+            for i in range(11):
+                if sensorColorLeft.color() == 2:
+                    contGreen += 1
+                else:
+                    contGreen += 0
+            if contGreen >= 9:
+                detectGreenLeft()
+            else:
+                motorMovementTurn.drive(0,-128)
+                while sensorColorRight.reflection() > 30:
+                    wait(10)
+                brick.display.clear()
+            contGreen = 0        
+        else:
+            brick.display.text("Right", (60, 50))
+            motorMovementTurn.drive(0,128)
+            while sensorColorLeft.reflection() < 30:
+                wait(10)
+            brick.display.clear() 
 
-    elif(sensorColorLeft.reflection() > 30 and sensorColorLeft.reflection() > 30):
+    elif sensorColorLeft.reflection() > 30 and sensorColorRight.reflection() < 30:
+        if sensorColorRight.color() == 2:
+            contGreen = 0
+            motorMovementForward.drive(0,0)
+            wait(1)
+            for i in range(11):
+                if sensorColorRight.color() == 2:
+                    contGreen += 1
+                else:
+                    contGreen += 0
+            if contGreen >= 9:
+                detectGreenRight()
+            else:
+                motorMovementTurn.drive(0,128)
+                while sensorColorLeft.reflection() > 30:
+                    wait(10)
+                brick.display.clear() 
+            contGreen = 0
+        else:
+            brick.display.text("Left", (60, 50))
+            motorMovementTurn.drive(0,-128)
+            while sensorColorRight.reflection() < 30:
+                wait(10)
+            brick.display.clear() 
+
+    elif sensorColorLeft.reflection() > 30 and sensorColorLeft.reflection() > 30:
         brick.display.text("White", (60, 50))
         motorMovementForward.drive(60,0)
         brick.display.clear()
-    
-    else:
-        brick.display.text("Black", (60, 50))
-        motorMovementForward.drive(20,0)
-        brick.display.clear()
-        
-# Function deflect obstacle
-#def deflectObstacle():
 
-# Function rescue
+    else:
+        if sensorColorLeft.color() == 2 and sensorColorRight.color() == 2:
+            contGreen = 0
+            motorMovementForward.drive(0,0)
+            for i in range(11):
+                if sensorColorLeft.color() == 2 and sensorColorRight.color() == 2:
+                    contGreen += 1
+                else:
+                    contGreen += 0
+            if contGreen >= 9:
+                doubleGreen()
+            else:
+                motorMovementForward.drive(20,0)
+
+        else:
+            brick.display.text("Black", (60, 50))
+            motorMovementForward.drive(20,0)
+            brick.display.clear()
+        
+# Function deflect obstacle ---------------------------------------------------------------
+def deflectObstacle():
+    motorMovementForward.drive(-100,0)
+    wait(512)
+
+# Function rescue -------------------------------------------------------------------------
 #def rescue():
 
-# Function detects gray
+# Function detects gray -------------------------------------------------------------------
 #def detectsGray():
 
+# Function main ---------------------------------------------------------------------------
 def main():
     while True:
-        followTrack()
+        if sensorUltrassonicFront.distance() < 100:
+            deflectObstacle()
+        else:
+            followTrack()
+
 main()
